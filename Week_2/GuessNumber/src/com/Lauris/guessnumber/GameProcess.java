0f17d335d629aa75ab582a62a60ae55c785d7c24
Lawrence_P <1997hm@oulook.com>
2017-07-22 22:37:19 +0800

    V1.3 更新注释，完善部分排行榜功能

--- Week_2/GuessNumber/src/com/Lauris/guessnumber/GameProcess.java
+++ Week_2/GuessNumber/src/com/Lauris/guessnumber/GameProcess.java
@@ -20,26 +20,35 @@ public class GameProcess {
             gameUI.gameOn(number);
             if (number == 1) {      //开始游戏
                 oneGame(newPlayer);
+                newPlayer = new Player();  //假设每次游戏都为新玩家，当游戏结束输入用户名相同则判断为同一玩家
             } else if (number == 2) { //重新开始 重复开始游戏的代码
-              oneGame(newPlayer);
+                oneGame(newPlayer);
+                newPlayer = new Player();
             } else if (number == 4) {
-
+                GuessNumber.getGuessNumber().showRank();
             }
         } while (number != 3);
     }
 
+    /**
+     *一次游戏过程的控制代码
+     */
     private static void oneGame(Player aPlayer) {
         GuessNumber gm = GuessNumber.getGuessNumber();
         Scanner input = new Scanner(System.in);
         gm.hasNumber();
+        //作弊，显示答案
         System.out.println(gm.cheat());
         int indexNumber = aPlayer.guessNumber();
         while (!gm.judge(indexNumber)) {
             indexNumber = aPlayer.guessNumber();
         }
         aPlayer.setBestWork(gm.getGuessTimes());
-        System.out.println("请输入您的昵称：(英文)");
-        if (input.hasNext())
+        gm.clearGuessTimes();
+        System.out.println("请输入您的昵称：");
+        if (input.hasNext()) {
             aPlayer.setPlayerName(input.next());
+        }
+        gm.addRank(aPlayer);
     }
 }
