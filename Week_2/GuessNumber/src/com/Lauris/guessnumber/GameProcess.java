f57f05ed8ede12ca6b966385bbda1f37a954a647
Lawrence_P <1997hm@oulook.com>
2017-07-22 13:16:51 +0800

    程序主体 V1.2 进一步封装

--- Week_2/GuessNumber/src/com/Lauris/guessnumber/GameProcess.java
+++ Week_2/GuessNumber/src/com/Lauris/guessnumber/GameProcess.java
@@ -9,31 +9,37 @@ import java.util.Scanner;
 public class GameProcess {
     public static void main(String[] args) {
         Player newPlayer = new Player();
-        GuessNumber gm = GuessNumber.getGuessNumber();
         GameUI gameUI = GameUI.getGameUI();
         Scanner input = new Scanner(System.in);
-        gameUI.show();
+
         //number控制游戏开关
-        int number = input.nextInt();
+        int number;
         do {
-            gameUI.gameOn(number);
-            if (number == 1) {      //开始游戏
-              gm.hasNumber();
-              int indexnumber = newPlayer.guessNumber();
-              while (!gm.judge(indexnumber)) {
-                  indexnumber = newPlayer.guessNumber();
-              }
-              newPlayer.setBestWork(gm.getGuessTimes());
-           } else if (number == 2) { //重新开始 重复开始游戏的代码
-              gm.hasNumber();
-              int indexnumber = newPlayer.guessNumber();
-              while (!gm.judge(indexnumber)) {
-                  indexnumber = newPlayer.guessNumber();
-              }
-              newPlayer.setBestWork(gm.getGuessTimes());
-            }
             gameUI.show();
             number = input.nextInt();
+            gameUI.gameOn(number);
+            if (number == 1) {      //开始游戏
+                oneGame(newPlayer);
+            } else if (number == 2) { //重新开始 重复开始游戏的代码
+              oneGame(newPlayer);
+            } else if (number == 4) {
+
+            }
         } while (number != 3);
+    }
+
+    private static void oneGame(Player aPlayer) {
+        GuessNumber gm = GuessNumber.getGuessNumber();
+        Scanner input = new Scanner(System.in);
+        gm.hasNumber();
+        System.out.println(gm.cheat());
+        int indexNumber = aPlayer.guessNumber();
+        while (!gm.judge(indexNumber)) {
+            indexNumber = aPlayer.guessNumber();
+        }
+        aPlayer.setBestWork(gm.getGuessTimes());
+        System.out.println("请输入您的昵称：(英文)");
+        if (input.hasNext())
+            aPlayer.setPlayerName(input.next());
     }
 }
