a94bb2d1f8a4e974968885f00bff575c920b476e
Lawrence_P <1997hm@oulook.com>
2017-07-22 13:20:59 +0800

    V1.2 添加获取名字的代码

--- Week_2/GuessNumber/src/com/Lauris/guessnumber/Player.java
+++ Week_2/GuessNumber/src/com/Lauris/guessnumber/Player.java
@@ -6,16 +6,19 @@ import java.util.Scanner;
  * Created by Lawrence on 2017/7/21.
  */
 public class Player {
-    private String playerName;
+    private String playerName = "Player";
     private int bestWork;
 
     public void setPlayerName(String name) {
         this.playerName = name ;
     }
 
+    public String getPlayerName() {return this.playerName;}
+
     public int getBestWork() {
         return this.bestWork;
     }
+
     public void setBestWork(int times) {
         this.bestWork = times;
     }
