77c4a40d4522ecb455b1cb6ef638e0f777a10b73
Lawrence_P <1997hm@oulook.com>
2017-08-08 22:35:57 +0800

    V1.5 更改了排行榜使用的容器，精简了排行榜的部分代码

--- Week_2/GuessNumber/src/com/Lauris/guessnumber/GuessNumber.java
+++ Week_2/GuessNumber/src/com/Lauris/guessnumber/GuessNumber.java
@@ -1,7 +1,6 @@
 package com.Lauris.guessnumber;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.*;
 
 /**
  * Created by Lawrence on 2017/7/21.
@@ -9,11 +8,14 @@ import java.util.Iterator;
 public class GuessNumber {
     private int trueNumber;
     private int guessTimes;
-    private static ArrayList<Player> rankList;
+    /*private static ArrayList<Player> rankList;
 
     {
         rankList = new ArrayList<>();
-    }
+    }*/
+
+    //由于排行榜的增删使用比较多，应该使用LinkedList而不是ArrayList
+    private LinkedList<Player> ranklist = new LinkedList<>();
 
     /**
     * 单例模式的练习
@@ -64,77 +66,65 @@ public class GuessNumber {
 
     /**
      *排行榜添加，维持功能，先添加再排序。
+     * 更新：之前对ArrayList的理解不够深刻，写了一些多余的代码，已经更正
      */
     public void addRank(Player aPlayer) {
-        ArrayList<Player> rankList = GuessNumber.rankList;
-        if (rankList.size() <= 10 ) {
-            if (hasEqualPlayer(aPlayer)) {
-                if (rankList.get(getEqualPlayer(aPlayer)).getBestWork() > aPlayer.getBestWork())
-                rankList.set(getEqualPlayer(aPlayer),aPlayer);
-            }
-            else if (rankList.size() < 10)
-                rankList.add(aPlayer);
-        }
+        ranklist.add(aPlayer);
         rankSort();
     }
+
     /**
      * 为排行榜提供排序功能
      */
     private void rankSort() {
-        for (int i = 0; i < rankList.size() - 1; i++) {
-            for (int j = 1; j < rankList.size() - i; j++) {
-                if (rankList.get(j-1).getBestWork() > rankList.get(j).getBestWork()) {
-                    Player temPlayer = rankList.get(j-1);
-                    rankList.set(j-1,rankList.get(j));
-                    rankList.set(j,temPlayer);
+        for (int i = 0; i < ranklist.size() - 1; i++) {
+            for (int j = 1; j < ranklist.size() - i; j++) {
+                if (ranklist.get(j-1).getBestWork() > ranklist.get(j).getBestWork()) {
+                    Player temPlayer = ranklist.get(j-1);
+                    ranklist.set(j-1,ranklist.get(j));
+                    ranklist.set(j,temPlayer);
                 }
             }
         }
     }
 
-    /**
-     * 是否有相同玩家
-     */
-    private boolean hasEqualPlayer(Player aPlayer) {
-        int temp = 0;
-        Iterator<Player> it = rankList.iterator();
-        while (it.hasNext()) {
-            if (it.next().getPlayerName().equals(aPlayer.getPlayerName())) {
-                temp++;
-                break;
-            }
-        }
-        return temp == 1;
-    }
 
     /**
      * 排行榜是否有相同玩家
+     * 因为排行榜允许一个玩家占据多个排名，所以用数字存储同一玩家的所有索引
      * @param aPlayer 玩家
-     * @return temp 返回相同玩家所在的索引
+     * @return temp 返回相同玩家所在的所有索引
      */
-    private int getEqualPlayer(Player aPlayer) {
-        int temp = 0;
-        Iterator<Player> it = rankList.iterator();
+    private int[] getEqualPlayer(Player aPlayer) {
+        int index = 0;
+        int i = 0;
+        int[] temp = new int[10];
+        Iterator<Player> it = ranklist.iterator();
         while (it.hasNext()) {
             if (it.next().getPlayerName().equals(aPlayer.getPlayerName())) {
+                temp[i++] = index;
                 break;
             }
-            temp++;
+            index++;
         }
         return temp;
     }
 
     /**
      * 排行榜显示控制
+     * 更新：排行榜只显示10个的限制从addRank()更改到此方法。
      */
     public void showRank() {
         System.out.printf("排名\t玩家名称\t猜测次数\n");
         int rankNumber = 1;
-        Iterator<Player> it = rankList.iterator();
+        Iterator<Player> it = ranklist.iterator();
         while (it.hasNext()) {
-            Player tmpPlayer =it.next();
-            System.out.printf("%d\t%s\t\t%d\n",rankNumber,tmpPlayer.getPlayerName(),tmpPlayer.getBestWork());
-            rankNumber++;
+            if (rankNumber <= 10) {
+                Player tmpPlayer = it.next();
+                System.out.printf("%d\t%s\t\t%d\n", rankNumber, tmpPlayer.getPlayerName(), tmpPlayer.getBestWork());
+                rankNumber++;
+            }
+            else break;
         }
     }
 }
